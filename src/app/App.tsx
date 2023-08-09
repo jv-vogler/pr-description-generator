4b9138b014cb6a1cc74d80d74477aff9bb928d16
jv-vogler <jvsvogler@gmail.com>
2023-08-08 22:26:46 -0300

    feat: make a temporary Hydration wrapper component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,20 @@
 import Form from '@/components/Form'
 import Preview from '@/components/Preview'
 import Markdown from '@/components/Markdown'
+import { HydrationZustand } from '@/components/HydrationZustand'
 
 const App = () => {
   return (
     <main className="flex justify-evenly">
-      <Form />
-      <Markdown />
-      <Preview />
+      <HydrationZustand>
+        <Form />
+      </HydrationZustand>
+      <HydrationZustand>
+        <Markdown />
+      </HydrationZustand>
+      <HydrationZustand>
+        <Preview />
+      </HydrationZustand>
     </main>
   )
 }
